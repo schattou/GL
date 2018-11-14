92f61865aaf2ac90c6766ae3f10618e17779c1ee
schattou <salah-eddine.chattou@u-psud.fr>
2018-11-14 20:57:43 +0100

    Bonsoir, c'est la partie 1 de l'exo 43

--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
 int main(void)
 {
-    unsigned long long int S1 = 1;
-    unsigned char n = 2;
-
-    while(n<50)
-    {
-        S1 = S1 * n;
-        n = n + 1;
-    }
-
-    return 0;
+	unsigned long int S1 = 3;
+	unsigned char n = 3;
+	while(S1<50)
+	{
+		S1 = S1+n;
+	}
+		 return 0;
 }
+		
