def6e118a3470d4c082ae72bc2f3acfa277dc2e6
schattou <salah-eddine.chattou@u-psud.fr>
2018-11-14 23:24:02 +0100

    voici ma partie final avec les 4 parties

--- src/main.c
+++ src/main.c
@@ -1,18 +1,34 @@
 #include "my_header.h"
-#include "stdlib.h"
+#include <stdlib.h>
+
 int main(void)
 {
-	unsigned long int S3 = 0;
-	unsigned char nb_rand; 
-	unsigned char i = 0;	 //ici n = nombre de termes
-while ( i < 12 )
-        
+    int s1=0 , n=3 , n1=3 , s3=0 , nb_rand =0 , i=1, s4=0, a=1 , d=1;
+    long unsigned int s2=0;
+    while(n1<36)	   // partie 1
     {
-        nb_rand = rand()%11;
-        
-        S3 = S3 + nb_rand ;
-        
-        i++;
-    
+        s1 = s1+n;
+        n = n+3;
     }
+    while(n1<=9876)	   // partie 2
+    {
+        s2= s2+n1;
+        n1= n1+3;
+    }
+    while(i<=12)			//partie 3
+    {
+        nb_rand = rand() %10 +1; // pour une valeur entre 1 et 10
+        s3= s3+nb_rand;
+        i++;
+    }
+    while(d<=20)	 // partie 4 (les 20 premier multiples) 
+    {
+        if(a%3==0 || a%7==0)
+        {
+            s4= s4+a;
+            d++;
+        }
+        a++;
+    }
+    return 0;
 }
