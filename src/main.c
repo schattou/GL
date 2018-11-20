60a00cbade35502e36152fe3815640acb7befd09
salah-eddine chattou <salah-eddine.chattou@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:28:12 +0100

    partie1

--- src/main.c
+++ src/main.c
@@ -5,5 +5,20 @@
 
 int main(void)
 {
+	int seuil;
+	int i;
+	unsigned char bin[10]={0, 1,  2, 3, 4, 5, 6, 7, 8, 9};
+	seuil = 9;
+	for (i=0 ; i<10 ; i++)
+	{
+						  if (bin[10]<seuil)
+	{
+		bin[i] = 0 ;
+						  }
+								   else
+						  {
+							  bin[i] = 1 ;
+						  }
+	}
     return 0;
 }
