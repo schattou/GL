8d03c23a1793d605e468bce88ca352bdd525eb17
Spooky Salah <44801429+schattou@users.noreply.github.com>
2018-11-21 23:34:38 +0100

    Exo 56
    Bonsoir voici l'exo 56

--- src/main.c
+++ src/main.c
@@ -1,39 +1,64 @@
-#include "TP_lib.h"
+#include <ansi_c.h>
+#include "my_header.h"
+#include <stdlib.h>
 
-void main (void)
+int dist[10] = {200 , 155 , 130 , 110 , 188, 130 , 185 , 139 , 117 , 112} ,miroir[6]={1,2,3};
+unsigned char bin[10] = {200 , 146 , 119 , 118 , 112, 122 , 147 , 150 , 123 , 111};
+short seuil = 155 ,seuilh =200,seuilb=-100, i,i2,i3,h,i4,i5;
+short sat[10], a[5]={1,5,9,8,11},b[5];
+
+int main(void)
 {
+for (i=0 ; i<10 ;i++)
+{
+if (bin[i] >= seuil )
+{
+bin[i] =1;
+}
+else 
+{
+bin[i] =0;
 
-    unsigned short int points = 0;
+}
+for (i2=0 ; i2<10 ;i2++)
+{
+sat[i2]=rand()%400-150;
+if (sat[i2]<seuilb)
+{
+sat[i2] = seuilb;
+}
 
-    unsigned short int resultat= 0;
+else if(sat[i2]>seuilh)
+{
+sat[i2] = seuilh;	
+}
+}
 
-
-    do
-
-    {
-        resultat = LancerDe ();
-
-
-         if ( resultat == 2 || resultat == 4 || resultat == 6)
-         {
-             points = points + resultat;
-         }
-
-         else if ( resultat == 3)
-
-         {
-
-             points = points * 2 ;
-
-         }
-
-         else
-
-         {
-         points = points - 2 ;
-         }
-    }
-
-    while (resultat != 1);
-
+for (i3=0 ; i3<10 ;i3++)
+{
+h=i3-1; 
+if (i3>0)
+{
+h=i3-1;
+while(h>=0)
+{
+dist[i3]=dist[i3]-dist[h];
+h--;
+}
+}
+else
+{
+dist[i3]=dist[i3];
+}
+}
+for(i4=0;i4<5;i4++)
+{
+b[i4]=a[4-i4];
+}
+for(i5=0;i5<3;i5++)
+{
+miroir[i5+3]=miroir[2-i5];
+}
+return 0;			
+}
 }
