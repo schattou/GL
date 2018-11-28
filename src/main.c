4d322f5a1a23152eeeb3ab2186a801ca8531830c
Spooky Salah <44801429+schattou@users.noreply.github.com>
2018-11-28 23:55:09 +0100

    Exo 58 fin
    Bonsoir monsieur, désolé de l'envoyé si tard mais j'avais un gros problème avec mes branches malgré l'aide de Semch nous n'avons pas réussis à savoir d'où cela viens.

--- src/main.c
+++ src/main.c
@@ -2,10 +2,41 @@
 //
 
 #include "my_header.h"
-#include "Tp_lib_suite.h"
-int main (void)
+#include "stdio.h"
+#include "TP_lib_suite.h"
+
+int main(void)
 {
-	unsigned char joueur[6]={ 2,49,5,28,35,18};
-	unsigned char loto[6]={
-	return 0;
+unsigned char joueur[6]={22,30,4,6,2,10},loto[6]={},i=0,h=0,i2=0,i3=0,i4=0,t=0,i5=0;
+
+for (i3=0;i3 <100;i3++)
+{
+initialiserTirage();
+for (i=0;i<6; i++)
+{
+loto[i]=tirerNumero();
+
+for(h=0;h<6;h++)
+{
+if(joueur[h]==loto[i])
+i2++;
+}
+
+if (h==6)
+{
+t=i2;
+i2=0;
+}
+}
+
+if (i4<t)
+{
+i4=t;
+i5=i3;
+}
+
+}	
+printf("Best score = %d Essaie = %d",i4,i5);
+getchar();
+    return 0;
 }
