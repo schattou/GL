e3147b1a4049846493ebe2f4aac2a8dde27d6fa3
Spooky Salah <44801429+schattou@users.noreply.github.com>
2018-12-07 21:39:22 +0100

    Update main.c
    désolé du petit retard

--- src/main.c
+++ src/main.c
@@ -3,9 +3,21 @@
 
 #include "my_header.h"
 #include "Tp_lib_suite.h"
-int main (void)
-{
-	unsigned char joueur[6]={ 2,49,5,28,35,18};
-	unsigned char loto[6]={
+ int main(void){
+	char phrase [100]= "Je m'appelle prenom nom et je suis ne en annee";
+	char nom [100];
+	char prenom [100];
+	unsigned long int annee=2000;
+	
+	printf("Nom SVP: ");
+	scanf("%s", nom);
+	printf("Prenom SVP: ");
+	scanf("%s", prenom);
+	printf("Annee SVP: ");
+	scanf("%lu", &annee);
+	sprintf(phrase, "Je m'appelle %s %s et je suis ne en %lu", prenom, nom, annee);
+	
+	printf ("%s\0", phrase);
+	
 	return 0;
 }
